8ad00c75db025a93e1a8e47e1dfd2ff44a78eb7b
GBLenin <lenin.gonzalezbueno@gmail.com>
2018-11-06 11:23:24 +0100

    gamba

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -44,7 +44,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(260, 68);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Genera";
+            this.button2.Text = "Gamba";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
